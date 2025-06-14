d14b6e1c1b425f6f68a62cc296a4aa53e4d44e9c
pinguinoadelia <gianmarco.delaurentiis@stud.unifi.it>
2025-06-15 01:17:35 +0200

    commit

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from products.models import Product  # importa il modello
 
-# Create your models here.
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)       
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)   
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
