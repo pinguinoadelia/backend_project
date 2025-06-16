4c353bff279b84d9b109489562e0e0426efc8fa8
pinguinoadelia <gianmarco.delaurentiis@stud.unifi.it>
2025-06-16 10:59:21 +0200

    commit

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -102,7 +102,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 STATICFILES_DIRS = [
